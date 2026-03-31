f658d48d4c0f336f547b8cf3d305bd98fc897b84
Tenaryo <tylertan@sjtu.edu.cn>
2026-03-31 12:02:48 +0000

    test: add --replicaof CLI parsing and slave role INFO tests

--- tests/test_info.cpp
+++ tests/test_info.cpp
@@ -44,11 +44,33 @@ void test_info_without_args_returns_replication_section() {
         << "\u2713 Test 2 passed: INFO without args returns replication section with role:master\n";
 }
 
+void test_info_replication_returns_role_slave_when_configured() {
+    Store store;
+    CommandHandler handler(store);
+    handler.set_role("slave");
+
+    std::string input = "*2\r\n$4\r\nINFO\r\n$11\r\nreplication\r\n";
+    auto response = handler.process(input);
+
+    assert(response.starts_with("$"));
+    auto crlf = response.find("\r\n");
+    assert(crlf != std::string::npos);
+    int len = std::stoi(response.substr(1, crlf - 1));
+    std::string content = response.substr(crlf + 2, len);
+
+    assert(content.find("# Replication") != std::string::npos);
+    assert(content.find("role:slave") != std::string::npos);
+
+    std::cout
+        << "\u2713 Test 3 passed: INFO replication returns role:slave when configured as slave\n";
+}
+
 int main() {
     std::cout << "Running INFO command tests...\n\n";
 
     test_info_replication_returns_role_master();
     test_info_without_args_returns_replication_section();
+    test_info_replication_returns_role_slave_when_configured();
 
     std::cout << "\n\u2713 All tests passed!\n";
     return 0;
