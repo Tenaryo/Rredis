30fc1713b9d7ff1d6f5291ec145e6f850ee3dc3d
Tenaryo <tylertan@sjtu.edu.cn>
2026-04-12 10:09:34 +0000

    test: add reproduction test for ZSCORE precision loss

--- tests/test_zscore.cpp
+++ tests/test_zscore.cpp
@@ -1,3 +1,4 @@
+#include "../src/handler/command_handler.hpp"
 #include "../src/store/store.hpp"
 #include <cassert>
 #include <iostream>
@@ -36,11 +37,27 @@ void test_zscore_returns_nullopt_for_missing() {
     std::cout << "✓ Test 2 passed: ZSCORE returns nullopt for missing key/member\n";
 }
 
+void test_zscore_high_precision_via_handler() {
+    Store store;
+    ServerConfig config;
+    CommandHandler handler(store, config);
+
+    handler.process(
+        "*4\r\n$4\r\nZADD\r\n$8\r\nzset_key\r\n$16\r\n27.9285743025792\r\n$4\r\nmem1\r\n");
+
+    auto response = handler.process("*3\r\n$6\r\nZSCORE\r\n$8\r\nzset_key\r\n$4\r\nmem1\r\n");
+
+    assert(response.find("27.9285743025792") != std::string::npos);
+
+    std::cout << "✓ Test 3 passed: ZSCORE preserves high precision score via handler\n";
+}
+
 int main() {
     std::cout << "Running ZSCORE command tests...\n\n";
 
     test_zscore_returns_correct_score();
     test_zscore_returns_nullopt_for_missing();
+    test_zscore_high_precision_via_handler();
 
     std::cout << "\n✓ All tests passed!\n";
     return 0;
