d33b53541e3d307c885d8b9df6f955feb9ad8902
Tenaryo <tylertan@sjtu.edu.cn>
2026-04-14 11:08:48 +0000

    test: add authentication enforcement tests for default user

--- tests/test_acl.cpp
+++ tests/test_acl.cpp
@@ -112,6 +112,85 @@ void test_auth_nonexistent_user_returns_wrongpass() {
     std::cout << "\u2713 Test passed: AUTH with nonexistent user returns WRONGPASS error\n";
 }
 
+void test_nopass_connection_auto_authenticated() {
+    Store store;
+    ServerConfig config;
+    CommandHandler handler(store, config);
+
+    auto result = handler.process_with_fd(10, "*2\r\n$3\r\nACL\r\n$6\r\nWHOAMI\r\n", nullptr);
+    assert(result.response == "$7\r\ndefault\r\n");
+
+    std::cout << "\u2713 Test passed: nopass default user auto-authenticates new connection\n";
+}
+
+void test_password_set_new_connection_gets_noauth() {
+    Store store;
+    ServerConfig config;
+    CommandHandler handler(store, config);
+
+    handler.process_with_fd(
+        10, "*4\r\n$3\r\nACL\r\n$7\r\nSETUSER\r\n$7\r\ndefault\r\n$11\r\n>mypassword\r\n", nullptr);
+
+    auto result = handler.process_with_fd(20, "*2\r\n$3\r\nACL\r\n$6\r\nWHOAMI\r\n", nullptr);
+    assert(result.response.starts_with("-NOAUTH"));
+
+    std::cout << "\u2713 Test passed: new connection gets NOAUTH after password is set\n";
+}
+
+void test_existing_connection_remains_authenticated() {
+    Store store;
+    ServerConfig config;
+    CommandHandler handler(store, config);
+
+    handler.process_with_fd(10, "*2\r\n$3\r\nACL\r\n$6\r\nWHOAMI\r\n", nullptr);
+
+    handler.process_with_fd(
+        10, "*4\r\n$3\r\nACL\r\n$7\r\nSETUSER\r\n$7\r\ndefault\r\n$11\r\n>mypassword\r\n", nullptr);
+
+    auto result = handler.process_with_fd(10, "*2\r\n$3\r\nACL\r\n$6\r\nWHOAMI\r\n", nullptr);
+    assert(result.response == "$7\r\ndefault\r\n");
+
+    std::cout << "\u2713 Test passed: existing authenticated connection remains authenticated\n";
+}
+
+void test_auth_success_allows_commands() {
+    Store store;
+    ServerConfig config;
+    CommandHandler handler(store, config);
+
+    handler.process_with_fd(
+        10, "*4\r\n$3\r\nACL\r\n$7\r\nSETUSER\r\n$7\r\ndefault\r\n$11\r\n>mypassword\r\n", nullptr);
+
+    auto auth_result =
+        handler.process_with_fd(20, "*3\r\n$4\r\nAUTH\r\n$7\r\ndefault\r\n$10\r\nmypassword\r\n", nullptr);
+    assert(auth_result.response == "+OK\r\n");
+
+    auto whoami_result =
+        handler.process_with_fd(20, "*2\r\n$3\r\nACL\r\n$6\r\nWHOAMI\r\n", nullptr);
+    assert(whoami_result.response == "$7\r\ndefault\r\n");
+
+    std::cout << "\u2713 Test passed: AUTH success allows subsequent commands\n";
+}
+
+void test_auth_failure_keeps_noauth() {
+    Store store;
+    ServerConfig config;
+    CommandHandler handler(store, config);
+
+    handler.process_with_fd(
+        10, "*4\r\n$3\r\nACL\r\n$7\r\nSETUSER\r\n$7\r\ndefault\r\n$11\r\n>mypassword\r\n", nullptr);
+
+    auto auth_result = handler.process_with_fd(
+        20, "*3\r\n$4\r\nAUTH\r\n$7\r\ndefault\r\n$12\r\nwrongpassword\r\n", nullptr);
+    assert(auth_result.response.starts_with("-WRONGPASS"));
+
+    auto whoami_result =
+        handler.process_with_fd(20, "*2\r\n$3\r\nACL\r\n$6\r\nWHOAMI\r\n", nullptr);
+    assert(whoami_result.response.starts_with("-NOAUTH"));
+
+    std::cout << "\u2713 Test passed: AUTH failure keeps connection unauthenticated\n";
+}
+
 int main() {
     std::cout << "Running ACL tests...\n\n";
 
@@ -122,6 +201,11 @@ int main() {
     test_auth_wrong_password_returns_wrongpass();
     test_auth_correct_password_returns_ok();
     test_auth_nonexistent_user_returns_wrongpass();
+    test_nopass_connection_auto_authenticated();
+    test_password_set_new_connection_gets_noauth();
+    test_existing_connection_remains_authenticated();
+    test_auth_success_allows_commands();
+    test_auth_failure_keeps_noauth();
 
     std::cout << "\n\u2713 All tests passed!\n";
     return 0;
