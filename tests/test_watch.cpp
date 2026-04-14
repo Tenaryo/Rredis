0f352abfc2d301d85c3f6a6e80f4953731309aaa
Tenaryo <tylertan@sjtu.edu.cn>
2026-04-14 15:43:35 +0000

    test: add UNWATCH command tests

--- tests/test_watch.cpp
+++ tests/test_watch.cpp
@@ -48,12 +48,52 @@ void test_watch_inside_multi_returns_error() {
     std::cout << "\u2713 Test 3 passed: WATCH inside MULTI returns error\n";
 }
 
+void test_unwatch_returns_ok() {
+    Store store;
+    CommandHandler handler(store);
+
+    auto result = handler.process_with_fd(1, "*1\r\n$8\r\nUNWATCH\r\n", nullptr);
+
+    assert(!result.should_block);
+    assert(result.response == "+OK\r\n");
+
+    std::cout << "\u2713 Test 4 passed: UNWATCH returns OK\n";
+}
+
+void test_unwatch_clears_watched_keys_then_exec_succeeds() {
+    Store store;
+    CommandHandler handler(store);
+
+    handler.process_with_fd(1, "*3\r\n$3\r\nSET\r\n$3\r\nfoo\r\n$3\r\n100\r\n", nullptr);
+    handler.process_with_fd(1, "*3\r\n$3\r\nSET\r\n$3\r\nbar\r\n$3\r\n200\r\n", nullptr);
+
+    handler.process_with_fd(1, "*3\r\n$5\r\nWATCH\r\n$3\r\nfoo\r\n$3\r\nbar\r\n", nullptr);
+
+    handler.process_with_fd(2, "*3\r\n$3\r\nSET\r\n$3\r\nfoo\r\n$3\r\n200\r\n", nullptr);
+
+    handler.process_with_fd(1, "*1\r\n$8\r\nUNWATCH\r\n", nullptr);
+
+    handler.process_with_fd(1, "*1\r\n$5\r\nMULTI\r\n", nullptr);
+    handler.process_with_fd(1, "*3\r\n$3\r\nSET\r\n$3\r\nfoo\r\n$3\r\n400\r\n", nullptr);
+
+    auto result = handler.process_with_fd(1, "*1\r\n$4\r\nEXEC\r\n", nullptr);
+    assert(result.response == "*1\r\n+OK\r\n");
+
+    auto foo_val = store.get("foo");
+    assert(foo_val.has_value());
+    assert(foo_val.value() == "400");
+
+    std::cout << "\u2713 Test 5 passed: UNWATCH clears watched keys, EXEC succeeds\n";
+}
+
 int main() {
-    std::cout << "Running WATCH command tests...\n\n";
+    std::cout << "Running WATCH/UNWATCH command tests...\n\n";
 
     test_watch_returns_ok();
     test_watch_without_key_returns_error();
     test_watch_inside_multi_returns_error();
+    test_unwatch_returns_ok();
+    test_unwatch_clears_watched_keys_then_exec_succeeds();
 
     std::cout << "\n\u2713 All tests passed!\n";
     return 0;
